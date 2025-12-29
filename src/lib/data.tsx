205f3d03628158f28dd3860e322ec4b1e585fdd1
nadeemcite <nady5730642@gmail.com>
2025-12-29 21:04:52 +0800

    fix: minor

--- src/lib/data.tsx
+++ src/lib/data.tsx
@@ -28,7 +28,8 @@ export const PAGES: Record<string, PageData> = {
         just for you.
       </>
     ),
-    quote: "\"Love is the only force capable of transforming an enemy into a friend.\""
+    quote: "\"Love is the only force capable of transforming an enemy into a friend.\"",
+    password: "abcd1234"
   },
   "ab2": {
     type: "music",
@@ -36,146 +37,166 @@ export const PAGES: Record<string, PageData> = {
     songName: "My Life",
     description: "Romantic Journey",
     colorTheme: "Ocean",
-    password: "12345"
+    password: "abcd1234"
   },
   "90e61069-aed4-402c-a275-1759b05ebb5f": {
     type: "music",
     audioUrl: "https://storage.googleapis.com/nadeem-public-gcs/mini/my_life.mp3",
     songName: "My Life",
     description: "Romantic Journey",
-    colorTheme: "Ocean"
+    colorTheme: "Ocean",
+    password: "abcd1234"
   },
   "b5e64f01-4095-6d1d-8da6-a5630ceddbf3": {
     type: "music",
     audioUrl: "https://storage.googleapis.com/nadeem-public-gcs/mini/my_life.mp3",
     songName: "Neon Lights",
     description: "Dance all night",
-    colorTheme: "Party"
+    colorTheme: "Party",
+    password: "abcd1234"
   },
   "4ca8c0d9-2e2f-6ae0-bb89-cd840b7ea0a7": {
     type: "music",
     audioUrl: "https://storage.googleapis.com/nadeem-public-gcs/mini/my_life.mp3",
     songName: "Sweet Nothing",
     description: "Just for you",
-    colorTheme: "Romantic"
+    colorTheme: "Romantic",
+    password: "abcd1234"
   },
   "101ab7ba-a47e-6c67-b82e-37d0111a51be": {
     type: "music",
     audioUrl: "https://storage.googleapis.com/nadeem-public-gcs/mini/my_life.mp3",
     songName: "Rainy Day",
     description: "Melancholy vibes",
-    colorTheme: "Sad"
+    colorTheme: "Sad",
+    password: "abcd1234"
   },
   "c1d320eb-0c55-6b1b-874f-31e1a077038d": {
     type: "music",
     audioUrl: "https://storage.googleapis.com/nadeem-public-gcs/mini/my_life.mp3",
     songName: "Sunshine",
     description: "Good morning",
-    colorTheme: "Happy"
+    colorTheme: "Happy",
+    password: "abcd1234"
   },
   "4ba6c253-5b2a-65de-9b38-62f0fcc8f885": {
     type: "music",
     audioUrl: "https://storage.googleapis.com/nadeem-public-gcs/mini/my_life.mp3",
     songName: "Deep Blue",
     description: "Calm waves",
-    colorTheme: "Ocean"
+    colorTheme: "Ocean",
+    password: "abcd1234"
   },
   "ec81b10c-95f3-6ec8-a4c6-48e4becb14ea": {
     type: "music",
     audioUrl: "https://storage.googleapis.com/nadeem-public-gcs/mini/my_life.mp3",
     songName: "Forever",
     description: "Eternity",
-    colorTheme: "Romantic"
+    colorTheme: "Romantic",
+    password: "abcd1234"
   },
   "896bc08b-c5ae-6af0-8db3-e70ab60a6123": {
     type: "music",
     audioUrl: "https://storage.googleapis.com/nadeem-public-gcs/mini/my_life.mp3",
     songName: "Club Mix",
     description: "Bass drop",
-    colorTheme: "Party"
+    colorTheme: "Party",
+    password: "abcd1234"
   },
   "e9aa4178-b045-6f8f-9074-0c6bf67ac752": {
     type: "music",
     audioUrl: "https://storage.googleapis.com/nadeem-public-gcs/mini/my_life.mp3",
     songName: "Smile",
     description: "Be happy",
-    colorTheme: "Happy"
+    colorTheme: "Happy",
+    password: "abcd1234"
   },
   "fd22b2f7-089c-63f1-a3a2-27f82c139d39": {
     type: "music",
     audioUrl: "https://storage.googleapis.com/nadeem-public-gcs/mini/my_life.mp3",
     songName: "Tears",
     description: "Broken heart",
-    colorTheme: "Sad"
+    colorTheme: "Sad",
+    password: "abcd1234"
   },
   "318f3edb-8c49-632e-b8fc-c2c99562a419": {
     type: "music",
     audioUrl: "https://storage.googleapis.com/nadeem-public-gcs/mini/my_life.mp3",
     songName: "Breeze",
     description: "Sea shore",
-    colorTheme: "Ocean"
+    colorTheme: "Ocean",
+    password: "abcd1234"
   },
   "88ff1b65-269a-6334-bb21-7fc438008684": {
     type: "music",
     audioUrl: "https://storage.googleapis.com/nadeem-public-gcs/mini/my_life.mp3",
     songName: "Valentine",
     description: "Be mine",
-    colorTheme: "Romantic"
+    colorTheme: "Romantic",
+    password: "abcd1234"
   },
   "f1c2312f-e3b6-6fa3-9952-9b1035bd4d68": {
     type: "music",
     audioUrl: "https://storage.googleapis.com/nadeem-public-gcs/mini/my_life.mp3",
     songName: "Celebration",
     description: "Cheers",
-    colorTheme: "Party"
+    colorTheme: "Party",
+    password: "abcd1234"
   },
   "0080a0fd-75b7-6cf4-b4ff-dea8537412af": {
     type: "music",
     audioUrl: "https://storage.googleapis.com/nadeem-public-gcs/mini/my_life.mp3",
     songName: "Joy",
     description: "Pure bliss",
-    colorTheme: "Happy"
+    colorTheme: "Happy",
+    password: "abcd1234"
   },
   "f01c040d-b558-6a60-b085-82ab182b6698": {
     type: "music",
     audioUrl: "https://storage.googleapis.com/nadeem-public-gcs/mini/my_life.mp3",
     songName: "Tides",
     description: "Moving water",
-    colorTheme: "Ocean"
+    colorTheme: "Ocean",
+    password: "abcd1234"
   },
   "db518420-c9ac-6577-a5d0-7b8e18486e22": {
     type: "music",
     audioUrl: "https://storage.googleapis.com/nadeem-public-gcs/mini/my_life.mp3",
     songName: "Lonely",
     description: "Solo night",
-    colorTheme: "Sad"
+    colorTheme: "Sad",
+    password: "abcd1234"
   },
   "c9292532-23b5-6ee6-a388-ed004ea6f1f2": {
     type: "music",
     audioUrl: "https://storage.googleapis.com/nadeem-public-gcs/mini/my_life.mp3",
     songName: "Adore",
     description: "Only you",
-    colorTheme: "Romantic"
+    colorTheme: "Romantic",
+    password: "abcd1234"
   },
   "024fe120-dc77-6bbe-bb14-517198761f6b": {
     type: "music",
     audioUrl: "https://storage.googleapis.com/nadeem-public-gcs/mini/my_life.mp3",
     songName: "Fiesta",
     description: "Let's go",
-    colorTheme: "Party"
+    colorTheme: "Party",
+    password: "abcd1234"
   },
   "79c76ca6-23ea-65d1-8b54-fb1ab9de1622": {
     type: "music",
     audioUrl: "https://storage.googleapis.com/nadeem-public-gcs/mini/my_life.mp3",
     songName: "Laugh",
     description: "Funny day",
-    colorTheme: "Happy"
+    colorTheme: "Happy",
+    password: "abcd1234"
   },
   "fa1fc415-a601-6f3f-9ce6-d7427daff434": {
     type: "music",
     audioUrl: "https://storage.googleapis.com/nadeem-public-gcs/mini/my_life.mp3",
     songName: "Coral",
     description: "Under the sea",
-    colorTheme: "Ocean"
+    colorTheme: "Ocean",
+    password: "abcd1234"
   }
 };
